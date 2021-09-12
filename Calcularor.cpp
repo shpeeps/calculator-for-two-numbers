fc4ef743a2b6d76420aa63dbfb751eb1a0be96bc
shpeeps <90564111+shpeeps@users.noreply.github.com>
2021-09-12 22:58:03 +0500

    Update 1
    Добавление произведения и частного чисел в программу

--- Calcularor.cpp
+++ Calcularor.cpp
@@ -3,20 +3,26 @@ using namespace std;
 
 int main()
 {
-    int x, y, sum, razn, sredn;
+    int x, y, sum, razn, sredn, proizv, chastn;
 
     setlocale(LC_ALL, "Russian");
 
     cout << "Введите два целых числа через пробел "; 
+    cout << "Введите два целых числа через пробел ";
     cin >> x >> y;
     sum = x + y;
     razn = x - y;
     sredn = (x + y) / 2;
+    proizv = x * y;
+    chastn = x / y;
+
     cout << "Сумма чисел " << sum << endl;
     cout << "Разность чисел " << razn << endl;
     cout << "Среднее арифметическое чисел " << sredn << endl;
-
+    cout << "Произведение чисел " << proizv << endl;
+    cout << "Частное чисел " << chastn << endl;
 
     system("pause");
     return 0; 
+
 }
